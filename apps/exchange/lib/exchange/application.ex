8fdc35964a66e02a8d6eff847617997eac3a4a8f
lmpn <a77763@alunos.uminho.pt>
2020-08-03 16:09:48 +0100

    Ticker automation
    Exchange application now launches matching engines depending on the
    config of the exchange app

--- apps/exchange/lib/exchange/application.ex
+++ apps/exchange/lib/exchange/application.ex
@@ -4,24 +4,33 @@ defmodule Exchange.Application do
   @moduledoc false
 
   use Application
+  import Supervisor.Spec
 
   def start(_type, _args) do
-    import Supervisor.Spec
 
-    children = [
-      supervisor(Registry, [:unique, :matching_engine_registry]),
-
-      # create a supervised Matching Enginge for AUXLND, AUXZUR
-      supervisor(
-        Exchange.MatchingEngine,
-        [[ticker: :AUXLND, currency: :GBP, min_price: 1000, max_price: 99_000]],
-        id: :AUXLND
-      )
-    ]
+    children =
+      [ supervisor(Registry, [:unique, :matching_engine_registry]) ] ++
+      Exchange.Application.create_tickers()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Exchange.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  def create_tickers() do
+    get_tickers_config()
+    |> Enum.map( fn {ticker, currency , min_price, max_price} ->
+      supervisor(
+        Exchange.MatchingEngine,
+          [[ticker: ticker, currency: currency, min_price: min_price, max_price: max_price]],
+          id: ticker
+        )
+    end)
+  end
+
+  def get_tickers_config() do
+    Application.get_env(:exchange, __MODULE__, [])[:tickers]
+  end
+
 end
