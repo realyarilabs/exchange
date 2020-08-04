0ac72a48b6a4d11b84bb3b6b372fdbaf4d400e55
teixeiramarcia <filipa.teixeira98@gmail.com>
2020-08-04 17:07:26 +0100

    Create adapter for EventBus inside Exchange app

--- apps/exchange/config/config.exs
+++ apps/exchange/config/config.exs
@@ -2,4 +2,4 @@ use Mix.Config
 config :exchange, Exchange.Application,
   tickers: [{:AUXLND, :GBP, 1000, 100_000}, {:AGUS, :USD, 1000, 100_000}]
 
-config :exchange, message_bus_adapter: EventBus, time_series_adapter: Flux
+config :exchange, message_bus_adapter: Exchange.Adapters.EventBus, time_series_adapter: Exchange.Adapters.Flux
